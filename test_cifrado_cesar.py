4e0e8fa7cf757e5d40847cd5f5ae4a8c0bd4bb3f
Angie <rodriguezangela95@gmail.com>
2025-02-11 14:52:19 +0100

    pytest and enviroment implementation

--- test_cifrado_cesar.py
+++ test_cifrado_cesar.py
@@ -1,11 +1,18 @@
 import cifrado_cesar
 
-encrypt2 = cifrado_cesar.encription(2)
-assert encrypt2("hola") == 'JQNC'
+def test_cifrado_simple_con_distancia():
+    assert cifrado_cesar.encrypt("hola, mundo",2) == "JQNC, ÑWOFQ"
+    assert cifrado_cesar.decrypt("JQNC, ÑWOFQ",2) == 'HOLA, MUNDO'
 
-encrypt2, decrypt2 = cifrado_cesar.par_encrypted(2)
-assert encrypt2("hola") == 'JQNC'
-assert decrypt2('JQNC') == "HOLA"
+def test_cifrado_establecido_con_distancia_2():
+    
+    encrypt2 = cifrado_cesar.encription(2)
+    assert encrypt2("hola") == 'JQNC'
 
-assert cifrado_cesar.encrypt("hola, mundo",2) == "JQNC, ÑWOFQ"
-assert cifrado_cesar.decrypt("JQNC, ÑWOFQ",2) == 'HOLA, MUNDO'
+def test_tupla_cifrado_descifrado_distancia_2():
+    
+    encrypt2, decrypt2 = cifrado_cesar.par_encrypted(2)
+    assert encrypt2("hola") == 'JQNC'
+    assert decrypt2('JQNC') == "HOLA"
+
+    
