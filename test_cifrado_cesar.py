5708c6737f0ad462914d6315f7b00ac27146a1ef
Angie <rodriguezangela95@gmail.com>
2025-02-11 13:21:13 +0100

    File correction

--- test_cifrado_cesar.py
+++ test_cifrado_cesar.py
@@ -1,11 +1,11 @@
-import cifrado_cesar2
+import cifrado_cesar
 
-encrypt2 = cifrado_cesar2.encription(2)
+encrypt2 = cifrado_cesar.encription(2)
 assert encrypt2("hola") == 'JQNC'
 
-encrypt2, decrypt2 = cifrado_cesar2.par_encrypted(2)
+encrypt2, decrypt2 = cifrado_cesar.par_encrypted(2)
 assert encrypt2("hola") == 'JQNC'
 assert decrypt2('JQNC') == "HOLA"
 
-assert cifrado_cesar2.encrypt("hola, mundo",2) == "JQNC, ÑWOFQ"
-assert cifrado_cesar2.decrypt("JQNC, ÑWOFQ",2) == 'HOLA, MUNDO'
+assert cifrado_cesar.encrypt("hola, mundo",2) == "JQNC, ÑWOFQ"
+assert cifrado_cesar.decrypt("JQNC, ÑWOFQ",2) == 'HOLA, MUNDO'
